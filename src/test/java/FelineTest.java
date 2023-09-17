882923af23744a80a22781358dff3f3c4274302b
Darya <kubasovada@mail.ru>
2023-09-17 23:05:53 +0300

    FelineTest added

---
+++ src/test/java/FelineTest.java
@@ -0,0 +1,41 @@
+import com.example.Feline;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FelineTest {
+
+    @Spy
+    Feline feline;
+
+    @Test
+    public void eatMeatReturnsCorrectValue() throws Exception {
+        Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        List<String> expected = List.of("Животные", "Птицы", "Рыба");
+        List<String> actual = feline.eatMeat();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getFamilyReturnsCorrectValue() {
+        Feline feline = new Feline();
+        String expected = "Кошачьи";
+        String actual = feline.getFamily();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getKittensReturnCorrectValue() {
+        feline.getKittens();
+        Mockito.verify(feline, Mockito.times(1)).getKittens(1);
+    }
+
+
+}
